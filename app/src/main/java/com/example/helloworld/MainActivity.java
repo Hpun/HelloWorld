22be7f08ebc99be46b439b4ca1b719f99645f4ea
Hpun <798077068@qq.com>
2020-05-25 14:56:12 +0800

    实现功能：点击登录按钮跳转至HomeActivity

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.helloworld.databinding.ActivityMainBinding;
 
@@ -21,8 +22,12 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //intent:意图分为显示和隐式
+                String phone =mBinding.editPhone.getText().toString();
+                String pwd=mBinding.editPwd.getText().toString();
+                if (phone.equals("10086")&&pwd.equals("123456")){
                 Intent intent=new Intent(MainActivity.this,HomeActivity.class);//上下文，目标activity得类
-                startActivity(intent);
+                startActivity(intent);}
+                else Toast.makeText(MainActivity.this,"手机号或密码错误",Toast.LENGTH_SHORT).show();
             }
         });
     }
