ac99901a33f706e4467045eeb9857c205cae7d10
Hpun <798077068@qq.com>
2020-05-25 15:10:54 +0800

    登录功能：从MainActivity传递手机号码给HomeActivity

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -26,7 +26,8 @@ public class MainActivity extends AppCompatActivity {
                 String pwd=mBinding.editPwd.getText().toString();
                 if (phone.equals("10086")&&pwd.equals("123456")){
                 Intent intent=new Intent(MainActivity.this,HomeActivity.class);//上下文，目标activity得类
-                startActivity(intent);}
+                intent.putExtra("data_phone","10086");
+                    startActivity(intent);}
                 else Toast.makeText(MainActivity.this,"手机号或密码错误",Toast.LENGTH_SHORT).show();
             }
         });
