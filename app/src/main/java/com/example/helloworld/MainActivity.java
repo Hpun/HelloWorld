7fbc16b791bfefc3236f403392d440ec09c4d5f0
Hpun <798077068@qq.com>
2020-05-29 11:01:21 +0800

    实现功能：界面跳转 已实现：注册保存数据逻辑 未实现：注册界面回调登录界面

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -24,13 +25,21 @@ public class MainActivity extends AppCompatActivity {
                 //intent:意图分为显示和隐式
                 String phone =mBinding.editPhone.getText().toString();
                 String pwd=mBinding.editPwd.getText().toString();
-                if (phone.equals("10086")&&pwd.equals("123456")){
+                SharedPreferences sp=getSharedPreferences("user_info",MODE_PRIVATE);
+                if (phone.equals(sp.getString("phone"+phone,"error"))&&pwd.equals(sp.getString("pwd"+phone,"error"))){
                 Intent intent=new Intent(MainActivity.this,HomeActivity.class);//上下文，目标activity得类
-                intent.putExtra("data_phone","10086");
+                intent.putExtra("data_phone",phone);
                     startActivity(intent);}
                 else {
                     Toast.makeText(MainActivity.this, "手机号或密码错误", Toast.LENGTH_SHORT).show();
                 }
+            }
+        });
+        mBinding.buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,RegisterActivity.class);
+                startActivity(intent);
             }
         });
     }
