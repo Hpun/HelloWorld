f66334d19c7f63ac5a47840e35f1c777e62d61a1
Hpun <798077068@qq.com>
2020-06-05 10:09:45 +0800

    HomeActivity实现功能：快速双击退出键，退出登录

--- app/src/main/java/com/example/helloworld/HomeActivity.java
+++ app/src/main/java/com/example/helloworld/HomeActivity.java
@@ -5,25 +5,50 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.helloworld.databinding.ActivityHomeBinding;
 
 public class HomeActivity extends AppCompatActivity {
 
     private ActivityHomeBinding mBinding;
+
+    private long exitTime=0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mBinding=ActivityHomeBinding.inflate(getLayoutInflater());
+        mBinding = ActivityHomeBinding.inflate(getLayoutInflater());
         setContentView(mBinding.getRoot());
 
-        Intent intent=getIntent();
+        Intent intent = getIntent();
         String phone = intent.getStringExtra("data_phone");
         mBinding.textphone.setText(phone);
-        SharedPreferences sp=getSharedPreferences("user_info",MODE_PRIVATE);
-        String textname = sp.getString("name"+phone,"0");
-        String textsex = sp.getString("sex"+phone,"0");
+        SharedPreferences sp = getSharedPreferences("user_info", MODE_PRIVATE);
+        String textname = sp.getString("name" + phone, "0");
+        String textsex = sp.getString("sex" + phone, "0");
         mBinding.textname.setText(textname);
         mBinding.textsex.setText(textsex);
+    }
+
+    /**
+     * 拦截系统返回键
+     */
+    @Override
+    public void onBackPressed() {
+        exit();
+    }
+
+    public void exit(){
+        long time = 2000;
+        if (System.currentTimeMillis()-exitTime>time){
+            //存储此次点击返回键的时间
+            exitTime=System.currentTimeMillis();
+            Toast.makeText(getApplicationContext(),"快速点击两次，可退出app",Toast.LENGTH_SHORT).show();
+        }else{
+            //完全退出app
+            finish();
+        }
+
     }
 }
