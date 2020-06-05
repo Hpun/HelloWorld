df79802a973283e6619b6dc7d1b86039ac3e7306
Hpun <798077068@qq.com>
2020-06-05 11:14:21 +0800

    HomeActivity实现功能：快速双击退出键，退出登录

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -27,8 +28,15 @@ public class MainActivity extends AppCompatActivity {
                 String pwd=mBinding.editPwd.getText().toString();
                 SharedPreferences sp=getSharedPreferences("user_info",MODE_PRIVATE);
                 if (phone.equals(sp.getString("phone"+phone,"error"))&&pwd.equals(sp.getString("pwd"+phone,"error"))){
-                Intent intent=new Intent(MainActivity.this,HomeActivity.class);//上下文，目标activity得类
-                intent.putExtra("data_phone",phone);
+                    Bundle bundle = new Bundle();
+                    String userName = sp.getString("name"+phone,"0");
+                    String userSms = "1".equals(sp.getString("sms"+phone,"0"))?"接受":"不接受";
+                    String userSex = sp.getString("sex"+phone,"0");
+                    UserInfo userInfo= new UserInfo(userName,pwd,userSex,phone,userSms);
+                    bundle.putSerializable("userInfo",userInfo);
+                    Intent intent=new Intent(MainActivity.this,HomeActivity.class);//上下文，目标activity得类
+                    intent.putExtras(bundle);
+                    //intent.putExtra("data_phone",phone);
                     startActivity(intent);}
                 else {
                     Toast.makeText(MainActivity.this, "手机号或密码错误", Toast.LENGTH_SHORT).show();
